26bcb8fa19ee0328b25f64aa3b091c567a1fd3b1
Michele Pittoni <pittoni@spaziodati.eu>
2022-12-26 20:13:11 +0100

    Gracefully handle request failures

--- main.go
+++ main.go
@@ -60,53 +60,78 @@ var (
 	codStazione = flag.String("stazione", "T0147", "Codice della stazione meteo, si veda anagrafica http://dati.meteotrentino.it/service.asmx/listaStazioni")
 	interval    = flag.Duration("intervallo", 60*time.Second, "Intervallo di tempo tra le richieste successive. I dati sono aggiornati alla fonte ogni 15 minuti")
 	listenAddr  = flag.String("listen-addr", ":8089", "Indirizzo di rete su cui esporre il server HTTP")
-	tempMetric  = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "temperature_celsius",
-		Help: "Current outside temperature in degrees Celsius",
-	})
-	rainMetric = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "rain_mm",
-		Help: "Amount of rain in the last period in mm",
-	})
-	humidityMetric = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "humidity_percent",
-		Help: "Relative himidity in percentage",
-	})
+	tempMetric  = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "temperature_celsius",
+			Help: "Current outside temperature in degrees Celsius",
+		},
+		[]string{"station_code", "place"},
+	)
+	rainMetric = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rain_mm",
+			Help: "Amount of rain in the last period in mm",
+		},
+		[]string{"station_code", "place"},
+	)
+	humidityMetric = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "humidity_percent",
+			Help: "Relative himidity in percentage",
+		},
+		[]string{"station_code", "place"},
+	)
 )
 
-func refresh() {
+func getRealTimeData() (item *DatiOggi, err error) {
 	res, err := http.Get(fmt.Sprintf(urlFmt, *codStazione))
 	if err != nil {
-		panic(err)
+		return
 	}
 	body, err := io.ReadAll(res.Body)
 	res.Body.Close()
 	if res.StatusCode > 299 {
-		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+		err = fmt.Errorf(
+			"Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
 	}
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 
-	o := DatiOggi{}
-	err = xml.Unmarshal(body, &o)
+	item = &DatiOggi{}
+	err = xml.Unmarshal(body, item)
 	if err != nil {
-		panic(err)
+		return
+	}
+	fmt.Println("Received and parsed data")
+	return
+}
+
+func refresh() {
+	labels := prometheus.Labels{
+		"station_code": *codStazione,
+		"place":        "Rovereto",
+	}
+	o, err := getRealTimeData()
+	if err != nil {
+		log.Println(err)
+		tempMetric.DeletePartialMatch(labels)
+		rainMetric.DeletePartialMatch(labels)
+		humidityMetric.DeletePartialMatch(labels)
 	}
 	// fmt.Printf("%#v\n", o)
-	fmt.Println("Received and parsed data")
 
 	temps := o.Temperature.TemperaturaAria
 	lastTemp := temps[len(temps)-1].Temperatura
-	tempMetric.Set(lastTemp)
+	tempMetric.With(labels).Set(lastTemp)
 
 	precs := o.Precipitazioni.Precipitazione
 	lastRain := precs[len(precs)-1].Pioggia
-	rainMetric.Set(lastRain)
+	rainMetric.With(labels).Set(lastRain)
 
 	hums := o.Umidita.Umidita
 	lastHum := hums[len(hums)-1].RH
-	humidityMetric.Set(lastHum)
+	humidityMetric.With(labels).Set(lastHum)
 }
 
 func main() {
